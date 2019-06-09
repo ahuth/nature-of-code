245d28a229015cb0ea12a5668c082807d62451ca
Andrew Huth <andrew@huth.me>
2019-06-08 21:50:35 -0700

    Add basic page and route layout

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './Home';
 
 export default function App() {
   return (
-    <main>
-      <span>hello world</span>
+    <main className="flex justify-center">
+      <Router>
+        <Route path="/" exact component={Home} />
+      </Router>
     </main>
   );
 }
---
+++ src/components/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>The Nature of Code</h1>
+      <p>List of projects written as while following along with <a href="https://natureofcode.com/">The Nature of Code</a>.</p>
+      <ol>
+        <li>None, yet!</li>
+      </ol>
+    </div>
+  );
+}
