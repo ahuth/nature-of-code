ca54bddc03a28e5403a18fadc8444ff4cc56856b
Andrew Huth <andrew@huth.me>
2019-06-09 07:38:47 -0700

    Setup routing correctly for the published deploys

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,10 +3,12 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './Home';
 import RandomWalk from './RandomWalk';
 
+const routingBasename = process.env.NODE_ENV === 'production' ? 'nature-of-code' : undefined;
+
 export default function App() {
   return (
     <main className="flex justify-center">
-      <Router>
+      <Router basename={routingBasename}>
         <Route path="/" exact component={Home} />
         <Route path="/RandomWalk" exact component={RandomWalk} />
       </Router>
