f84af4df3848584a047367648707489ba4a89c5c
Andrew Huth <andrew@huth.me>
2019-06-17 02:56:49 +0200

    Words

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div>
       <h1>The Nature of Code</h1>
-      <p>List of projects written as while following along with <a href="https://natureofcode.com/">The Nature of Code</a>.</p>
+      <p>List of projects written while following along with <a href="https://natureofcode.com/">The Nature of Code</a>.</p>
       <ol>
         <li><Link to="/RandomWalk">Random Walk</Link></li>
         <li><Link to="/Gaussian">Gaussian</Link></li>
